53b0458b4d4f9053d432aefcf3b7759211ec2b19
danel <danel@DESKTOP-3DCJVBQ>
2020-09-19 15:02:10 +0200

    anyadir comentarios, faltan todavia comentarios

--- Practica0/src/Main.java
+++ Practica0/src/Main.java
@@ -1,6 +1,12 @@
 
 import visuales.Ventana;
 
+/**
+ * Main principal que ejecuta el programa
+ * 
+ * @author danel
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		Ventana vn = new Ventana();
