c4953c5d83c369b41dd178d32ad3ffb7fac35e69
danel <danel@DESKTOP-3DCJVBQ>
2020-10-28 20:53:01 +0100

    Avance hasta paso 2 (Metido JUnit)

--- Practica1/src/ud/prog3/pr01/ListaDeReproduccion.java
+++ Practica1/src/ud/prog3/pr01/ListaDeReproduccion.java
@@ -6,107 +6,197 @@ import javax.swing.ListModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
-
-/** Clase para crear instancias como listas de reproducción,
- * que permite almacenar listas de ficheros con posición de índice
- * (al estilo de un array / arraylist)
- * con marcas de error en los ficheros y con métodos para cambiar la posición
- * de los elementos en la lista, borrar elementos y añadir nuevos.
+/**
+ * Clase para crear instancias como listas de reproducciï¿½n, que permite
+ * almacenar listas de ficheros con posiciï¿½n de ï¿½ndice (al estilo de un array /
+ * arraylist) con marcas de error en los ficheros y con mï¿½todos para cambiar la
+ * posiciï¿½n de los elementos en la lista, borrar elementos y aï¿½adir nuevos.
  */
 public class ListaDeReproduccion implements ListModel<String> {
-	ArrayList<File> ficherosLista;     // ficheros de la lista de reproducción
-	int ficheroEnCurso = -1;           // Fichero seleccionado (-1 si no hay ninguno seleccionado)
-	
-	/** Devuelve uno de los ficheros de la lista
-	 * @param posi	Posición del fichero en la lista (de 0 a size()-1)
-	 * @return	Devuelve el fichero en esa posición
-	 * @throws IndexOutOfBoundsException	Si el índice no es válido
-	 */
-	public File getFic( int posi ) throws IndexOutOfBoundsException {
-		return ficherosLista.get( posi );
-	}	
+	ArrayList<File> ficherosLista; // ficheros de la lista de reproducciï¿½n
+	int ficheroEnCurso = -1; // Fichero seleccionado (-1 si no hay ninguno seleccionado)
 
-	/** Añade a la lista de reproducción todos los ficheros que haya en la 
-	 * carpeta indicada, que cumplan el filtro indicado.
-	 * Si hay cualquier error, la lista de reproducción queda solo con los ficheros
-	 * que hayan podido ser cargados de forma correcta.
-	 * @param carpetaFicheros	Path de la carpeta donde buscar los ficheros
-	 * @param filtroFicheros	Filtro del formato que tienen que tener los nombres de
-	 * 							los ficheros para ser cargados.
-	 * 							String con cualquier letra o dígito. Si tiene un asterisco
-	 * 							hace referencia a cualquier conjunto de letras o dígitos.
-	 * 							Por ejemplo p*.* hace referencia a cualquier fichero de nombre
-	 * 							que empiece por p y tenga cualquier extensión.
-	 * @return	Número de ficheros que han sido añadidos a la lista
+	/**
+	 * Constructor de la lista de reproduccion que crea una lista de reproduccion
+	 * vacia
+	 */
+	public ListaDeReproduccion() {
+		this.clear();
+	}
+
+	/**
+	 * Intercambia dos elementos de la lista de reproduccion, si cualquiera de las
+	 * dos posiciones pasadas como parametro no es valida no hace nada
+	 * 
+	 * @param posi1 posicion en la lista del primer elemento
+	 * @param posi2 posicion en la lista del primer elemento
+	 */
+	public void intercambia(int posi1, int posi2) {
+		try {
+			// Me aseguro que ambas posiciones estan gracias a que sino salta una excepcion
+			File f1 = ficherosLista.get(posi1);
+			File f2 = ficherosLista.get(posi2);
+
+			// Hago el intercambio
+			ficherosLista.remove(posi1);
+			ficherosLista.add(posi1, f2);
+
+			ficherosLista.remove(posi2);
+			ficherosLista.add(posi2, f1);
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return;
+		}
+	}
+
+	/**
+	 * Devuelve un entero con el tamanyo de la lista de reproduccion
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return ficherosLista.size();
+	}
+
+	/**
+	 * Anyade un fichero a la lista en ultima posicion
+	 * 
+	 * @param file
+	 */
+	public void add(File f) {
+		ficherosLista.add(f);
+	}
+
+	/**
+	 * Elimina el fichero indicado en la posicion pasada como parametro Si la
+	 * posicion no es una posicion valida no hace nada
+	 * 
+	 * @param pos
+	 */
+	public void removeFic(int pos) {
+		try {
+			ficherosLista.remove(pos);
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+	/**
+	 * Borra todos los elementos de la lista
+	 */
+	public void clear() {
+		ficherosLista = new ArrayList<>();
+	}
+
+	/**
+	 * Devuelve uno de los ficheros de la lista
+	 * 
+	 * @param posi Posiciï¿½n del fichero en la lista (de 0 a size()-1)
+	 * @return Devuelve el fichero en esa posiciï¿½n
+	 * @throws IndexOutOfBoundsException Si el ï¿½ndice no es vï¿½lido
+	 */
+	public File getFic(int posi) throws IndexOutOfBoundsException {
+		return ficherosLista.get(posi);
+	}
+
+	/**
+	 * Aï¿½ade a la lista de reproducciï¿½n todos los ficheros que haya en la carpeta
+	 * indicada, que cumplan el filtro indicado. Si hay cualquier error, la lista de
+	 * reproducciï¿½n queda solo con los ficheros que hayan podido ser cargados de
+	 * forma correcta.
+	 * 
+	 * @param carpetaFicheros Path de la carpeta donde buscar los ficheros
+	 * @param filtroFicheros  Filtro del formato que tienen que tener los nombres de
+	 *                        los ficheros para ser cargados. String con cualquier
+	 *                        letra o dï¿½gito. Si tiene un asterisco hace referencia
+	 *                        a cualquier conjunto de letras o dï¿½gitos. Por ejemplo
+	 *                        p*.* hace referencia a cualquier fichero de nombre que
+	 *                        empiece por p y tenga cualquier extensiï¿½n.
+	 * @return Nï¿½mero de ficheros que han sido aï¿½adidos a la lista
 	 */
 	public int add(String carpetaFicheros, String filtroFicheros) {
-		// TODO: Codificar este método de acuerdo a la práctica (pasos 3 y sucesivos)
-		filtroFicheros = filtroFicheros.replaceAll( "\\.", "\\\\." );  // Pone el símbolo de la expresión regular \. donde figure un .
+		// TODO: Codificar este mï¿½todo de acuerdo a la prï¿½ctica (pasos 3 y sucesivos)
+		filtroFicheros = filtroFicheros.replaceAll("\\.", "\\\\."); // Pone el sï¿½mbolo de la expresiï¿½n regular \. donde
+																	// figure un .
 		return 0;
 	}
-	
-	
+
 	//
-	// Métodos de selección
+	// Mï¿½todos de selecciï¿½n
 	//
-	
-	/** Seleciona el primer fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+
+	/**
+	 * Seleciona el primer fichero de la lista de reproducciï¿½n
+	 * 
+	 * @return true si la selecciï¿½n es correcta, false si hay error y no se puede
+	 *         seleccionar
 	 */
 	public boolean irAPrimero() {
-		ficheroEnCurso = 0;  // Inicia
-		if (ficheroEnCurso>=ficherosLista.size()) {
-			ficheroEnCurso = -1;  // Si no se encuentra, no hay selección
-			return false;  // Y devuelve error
+		ficheroEnCurso = 0; // Inicia
+		if (ficheroEnCurso >= ficherosLista.size()) {
+			ficheroEnCurso = -1; // Si no se encuentra, no hay selecciï¿½n
+			return false; // Y devuelve error
 		}
 		return true;
 	}
-	
-	/** Seleciona el último fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+
+	/**
+	 * Seleciona el ï¿½ltimo fichero de la lista de reproducciï¿½n
+	 * 
+	 * @return true si la selecciï¿½n es correcta, false si hay error y no se puede
+	 *         seleccionar
 	 */
 	public boolean irAUltimo() {
-		ficheroEnCurso = ficherosLista.size()-1;  // Inicia al final
-		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selección
-			return false;  // Y devuelve error
+		ficheroEnCurso = ficherosLista.size() - 1; // Inicia al final
+		if (ficheroEnCurso == -1) { // Si no se encuentra, no hay selecciï¿½n
+			return false; // Y devuelve error
 		}
 		return true;
 	}
 
-	/** Seleciona el anterior fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/**
+	 * Seleciona el anterior fichero de la lista de reproducciï¿½n
+	 * 
+	 * @return true si la selecciï¿½n es correcta, false si hay error y no se puede
+	 *         seleccionar
 	 */
 	public boolean irAAnterior() {
-		if (ficheroEnCurso>=0) ficheroEnCurso--;
-		if (ficheroEnCurso==-1) {  // Si no se encuentra, no hay selección
-			return false;  // Y devuelve error
+		if (ficheroEnCurso >= 0)
+			ficheroEnCurso--;
+		if (ficheroEnCurso == -1) { // Si no se encuentra, no hay selecciï¿½n
+			return false; // Y devuelve error
 		}
 		return true;
 	}
 
-	/** Seleciona el siguiente fichero de la lista de reproducción
-	 * @return	true si la selección es correcta, false si hay error y no se puede seleccionar
+	/**
+	 * Seleciona el siguiente fichero de la lista de reproducciï¿½n
+	 * 
+	 * @return true si la selecciï¿½n es correcta, false si hay error y no se puede
+	 *         seleccionar
 	 */
 	public boolean irASiguiente() {
 		ficheroEnCurso++;
-		if (ficheroEnCurso>=ficherosLista.size()) {
-			ficheroEnCurso = -1;  // Si no se encuentra, no hay selección
-			return false;  // Y devuelve error
+		if (ficheroEnCurso >= ficherosLista.size()) {
+			ficheroEnCurso = -1; // Si no se encuentra, no hay selecciï¿½n
+			return false; // Y devuelve error
 		}
 		return true;
 	}
 
-	/** Devuelve el fichero seleccionado de la lista
-	 * @return	Posición del fichero seleccionado en la lista de reproducción (0 a n-1), -1 si no lo hay
+	/**
+	 * Devuelve el fichero seleccionado de la lista
+	 * 
+	 * @return Posiciï¿½n del fichero seleccionado en la lista de reproducciï¿½n (0 a
+	 *         n-1), -1 si no lo hay
 	 */
 	public int getFicSeleccionado() {
 		return ficheroEnCurso;
 	}
 
 	//
-	// Métodos de DefaultListModel
+	// Mï¿½todos de DefaultListModel
 	//
-	
+
 	@Override
 	public int getSize() {
 		return ficherosLista.size();
@@ -117,23 +207,24 @@ public class ListaDeReproduccion implements ListModel<String> {
 		return ficherosLista.get(index).getName();
 	}
 
-		// Escuchadores de datos de la lista
-		ArrayList<ListDataListener> misEscuchadores = new ArrayList<>();
+	// Escuchadores de datos de la lista
+	ArrayList<ListDataListener> misEscuchadores = new ArrayList<>();
+
 	@Override
 	public void addListDataListener(ListDataListener l) {
-		misEscuchadores.add( l );
+		misEscuchadores.add(l);
 	}
 
 	@Override
 	public void removeListDataListener(ListDataListener l) {
-		misEscuchadores.remove( l );
+		misEscuchadores.remove(l);
 	}
-	
-	// Llamar a este método cuando se añada un elemento a la lista
+
+	// Llamar a este mï¿½todo cuando se aï¿½ada un elemento a la lista
 	// (Utilizado para avisar a los escuchadores de cambio de datos de la lista)
-	private void avisarAnyadido( int posi ) {
+	private void avisarAnyadido(int posi) {
 		for (ListDataListener ldl : misEscuchadores) {
-			ldl.intervalAdded( new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, posi, posi ));
+			ldl.intervalAdded(new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, posi, posi));
 		}
 	}
 }
--- Practica1/src/ud/prog3/pr01/VideoPlayer.java
+++ Practica1/src/ud/prog3/pr01/VideoPlayer.java
@@ -15,88 +15,91 @@ import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
 import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
 
-/** Ventana principal de reproductor de vídeo
- * Utiliza la librería VLCj que debe estar instalada y configurada
- *     (http://www.capricasoftware.co.uk/projects/vlcj/index.html)
- * @author Andoni Eguíluz Morán
- * Facultad de Ingeniería - Universidad de Deusto
+/**
+ * Ventana principal de reproductor de vï¿½deo Utiliza la librerï¿½a VLCj que debe
+ * estar instalada y configurada
+ * (http://www.capricasoftware.co.uk/projects/vlcj/index.html)
+ * 
+ * @author Andoni Eguï¿½luz Morï¿½n Facultad de Ingenierï¿½a - Universidad de Deusto
  */
 public class VideoPlayer extends JFrame {
 	private static final long serialVersionUID = 1L;
-	
+
 	// Varible de ventana principal de la clase
 	private static VideoPlayer miVentana;
 
 	// Atributo de VLCj
 	private EmbeddedMediaPlayerComponent mediaPlayerComponent;
 	// Atributos manipulables de swing
-	private JList<String> lCanciones = null;  // Lista vertical de vídeos del player
-	private JProgressBar pbVideo = null;      // Barra de progreso del vídeo en curso
-	private JCheckBox cbAleatorio = null;     // Checkbox de reproducción aleatoria
-	private JLabel lMensaje = null;           // Label para mensaje de reproducción
+	private JList<String> lCanciones = null; // Lista vertical de vï¿½deos del player
+	private JProgressBar pbVideo = null; // Barra de progreso del vï¿½deo en curso
+	private JCheckBox cbAleatorio = null; // Checkbox de reproducciï¿½n aleatoria
+	private JLabel lMensaje = null; // Label para mensaje de reproducciï¿½n
 	// Datos asociados a la ventana
-	private ListaDeReproduccion listaRepVideos;  // Modelo para la lista de vídeos
+	private ListaDeReproduccion listaRepVideos; // Modelo para la lista de vï¿½deos
 
 	public VideoPlayer() {
-		// Creación de datos asociados a la ventana (lista de reproducción)
+		// Creaciï¿½n de datos asociados a la ventana (lista de reproducciï¿½n)
 		listaRepVideos = new ListaDeReproduccion();
-		
-		// Creación de componentes/contenedores de swing
-		lCanciones = new JList<String>( listaRepVideos );
-		pbVideo = new JProgressBar( 0, 10000 );
-		cbAleatorio = new JCheckBox("Rep. aleatoria");
-		lMensaje = new JLabel( "" );
-		JPanel pBotonera = new JPanel();
-		JButton bAnyadir = new JButton( new ImageIcon( VideoPlayer.class.getResource("img/Button Add.png")) );
-		JButton bAtras = new JButton( new ImageIcon( VideoPlayer.class.getResource("img/Button Rewind.png")) );
-		JButton bPausaPlay = new JButton( new ImageIcon( VideoPlayer.class.getResource("img/Button Play Pause.png")) );
-		JButton bAdelante = new JButton( new ImageIcon( VideoPlayer.class.getResource("img/Button Fast Forward.png")) );
-		JButton bMaximizar = new JButton( new ImageIcon( VideoPlayer.class.getResource("img/Button Maximize.png")) );
-		
-		// Componente de VCLj
-        mediaPlayerComponent = new EmbeddedMediaPlayerComponent() {
-			private static final long serialVersionUID = 1L;
-			@Override
-            protected FullScreenStrategy onGetFullScreenStrategy() {
-                return new Win32FullScreenStrategy(VideoPlayer.this);
-            }
-        };
 
-		// Configuración de componentes/contenedores
-		setTitle("Video Player - Deusto Ingeniería");
-		setLocationRelativeTo( null );  // Centra la ventana en la pantalla
-		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		setSize( 800, 600 );
-		lCanciones.setPreferredSize( new Dimension( 200,  500 ) );
-		pBotonera.setLayout( new FlowLayout( FlowLayout.LEFT ));
-		
+		// Creaciï¿½n de componentes/contenedores de swing
+		lCanciones = new JList<String>(listaRepVideos);
+		pbVideo = new JProgressBar(0, 10000);
+		cbAleatorio = new JCheckBox("Rep. aleatoria");
+		lMensaje = new JLabel("");
+		JPanel pBotonera = new JPanel();
+		JButton bAnyadir = new JButton(new ImageIcon(VideoPlayer.class.getResource("img/Button Add.png")));
+		JButton bAtras = new JButton(new ImageIcon(VideoPlayer.class.getResource("img/Button Rewind.png")));
+		JButton bPausaPlay = new JButton(new ImageIcon(VideoPlayer.class.getResource("img/Button Play Pause.png")));
+		JButton bAdelante = new JButton(new ImageIcon(VideoPlayer.class.getResource("img/Button Fast Forward.png")));
+		JButton bMaximizar = new JButton(new ImageIcon(VideoPlayer.class.getResource("img/Button Maximize.png")));
+
+		// Componente de VCLj
+		mediaPlayerComponent = new EmbeddedMediaPlayerComponent() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected FullScreenStrategy onGetFullScreenStrategy() {
+				return new Win32FullScreenStrategy(VideoPlayer.this);
+			}
+		};
+
+		// Configuraciï¿½n de componentes/contenedores
+		setTitle("Video Player - Deusto Ingenierï¿½a");
+		setLocationRelativeTo(null); // Centra la ventana en la pantalla
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(800, 600);
+		lCanciones.setPreferredSize(new Dimension(200, 500));
+		pBotonera.setLayout(new FlowLayout(FlowLayout.LEFT));
+
 		// Enlace de componentes y contenedores
-		pBotonera.add( bAnyadir );
-		pBotonera.add( bAtras );
-		pBotonera.add( bPausaPlay );
-		pBotonera.add( bAdelante );
-		pBotonera.add( bMaximizar );
-		pBotonera.add( cbAleatorio );
-		pBotonera.add( lMensaje );
-		getContentPane().add( mediaPlayerComponent, BorderLayout.CENTER );
-		getContentPane().add( pBotonera, BorderLayout.NORTH );
-		getContentPane().add( pbVideo, BorderLayout.SOUTH );
-		getContentPane().add( new JScrollPane( lCanciones ), BorderLayout.WEST );
-		
+		pBotonera.add(bAnyadir);
+		pBotonera.add(bAtras);
+		pBotonera.add(bPausaPlay);
+		pBotonera.add(bAdelante);
+		pBotonera.add(bMaximizar);
+		pBotonera.add(cbAleatorio);
+		pBotonera.add(lMensaje);
+		getContentPane().add(mediaPlayerComponent, BorderLayout.CENTER);
+		getContentPane().add(pBotonera, BorderLayout.NORTH);
+		getContentPane().add(pbVideo, BorderLayout.SOUTH);
+		getContentPane().add(new JScrollPane(lCanciones), BorderLayout.WEST);
+
 		// Escuchadores
-		bAnyadir.addActionListener( new ActionListener() {
+		bAnyadir.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				File fPath = pedirCarpeta();
-				if (fPath==null) return;
+				if (fPath == null)
+					return;
 				path = fPath.getAbsolutePath();
 				// TODO: pedir ficheros por ventana de entrada (JOptionPane)
 				// ficheros = ...
-				listaRepVideos.add( path, ficheros );
+				listaRepVideos.add(path, ficheros);
 				lCanciones.repaint();
 			}
 		});
-		bAtras.addActionListener( new ActionListener() {
+		bAtras.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				paraVideo();
@@ -104,7 +107,7 @@ public class VideoPlayer extends JFrame {
 				lanzaVideo();
 			}
 		});
-		bAdelante.addActionListener( new ActionListener() {
+		bAdelante.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				paraVideo();
@@ -112,7 +115,7 @@ public class VideoPlayer extends JFrame {
 				lanzaVideo();
 			}
 		});
-		bPausaPlay.addActionListener( new ActionListener() {
+		bPausaPlay.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (mediaPlayerComponent.getMediaPlayer().isPlayable()) {
@@ -126,127 +129,132 @@ public class VideoPlayer extends JFrame {
 				}
 			}
 		});
-		bMaximizar.addActionListener( new ActionListener() {
+		bMaximizar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (mediaPlayerComponent.getMediaPlayer().isFullScreen())
-			        mediaPlayerComponent.getMediaPlayer().setFullScreen(false);
+					mediaPlayerComponent.getMediaPlayer().setFullScreen(false);
 				else
 					mediaPlayerComponent.getMediaPlayer().setFullScreen(true);
 			}
 		});
-		addWindowListener( new WindowAdapter() {
+		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				mediaPlayerComponent.getMediaPlayer().stop();
 				mediaPlayerComponent.getMediaPlayer().release();
 			}
 		});
-		mediaPlayerComponent.getMediaPlayer().addMediaPlayerEventListener( 
-			new MediaPlayerEventAdapter() {
-				@Override
-				public void finished(MediaPlayer mediaPlayer) {
-					listaRepVideos.irASiguiente();
-					lanzaVideo();
-				}
-				@Override
-				public void error(MediaPlayer mediaPlayer) {
-					listaRepVideos.irASiguiente();
-					lanzaVideo();
-					lCanciones.repaint();
-				}
-			    @Override
-			    public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
-					pbVideo.setValue( (int) (10000.0 * 
-							mediaPlayerComponent.getMediaPlayer().getTime() /
-							mediaPlayerComponent.getMediaPlayer().getLength()) );
-					pbVideo.repaint();
-			    }
+		mediaPlayerComponent.getMediaPlayer().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+			@Override
+			public void finished(MediaPlayer mediaPlayer) {
+				listaRepVideos.irASiguiente();
+				lanzaVideo();
+			}
+
+			@Override
+			public void error(MediaPlayer mediaPlayer) {
+				listaRepVideos.irASiguiente();
+				lanzaVideo();
+				lCanciones.repaint();
+			}
+
+			@Override
+			public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
+				pbVideo.setValue((int) (10000.0 * mediaPlayerComponent.getMediaPlayer().getTime()
+						/ mediaPlayerComponent.getMediaPlayer().getLength()));
+				pbVideo.repaint();
+			}
 		});
 	}
 
 	//
-	// Métodos sobre el player de vídeo
+	// Mï¿½todos sobre el player de vï¿½deo
 	//
-	
-	// Para la reproducción del vídeo en curso
+
+	// Para la reproducciï¿½n del vï¿½deo en curso
 	private void paraVideo() {
-		if (mediaPlayerComponent.getMediaPlayer()!=null)
+		if (mediaPlayerComponent.getMediaPlayer() != null)
 			mediaPlayerComponent.getMediaPlayer().stop();
 	}
 
-	// Empieza a reproducir el vídeo en curso de la lista de reproducción
+	// Empieza a reproducir el vï¿½deo en curso de la lista de reproducciï¿½n
 	private void lanzaVideo() {
-		if (mediaPlayerComponent.getMediaPlayer()!=null &&
-			listaRepVideos.getFicSeleccionado()!=-1) {
+		if (mediaPlayerComponent.getMediaPlayer() != null && listaRepVideos.getFicSeleccionado() != -1) {
 			File ficVideo = listaRepVideos.getFic(listaRepVideos.getFicSeleccionado());
-			mediaPlayerComponent.getMediaPlayer().playMedia( 
-				ficVideo.getAbsolutePath() );
-			lCanciones.setSelectedIndex( listaRepVideos.getFicSeleccionado() );
+			mediaPlayerComponent.getMediaPlayer().playMedia(ficVideo.getAbsolutePath());
+			lCanciones.setSelectedIndex(listaRepVideos.getFicSeleccionado());
 		} else {
-			lCanciones.setSelectedIndices( new int[] {} );
+			lCanciones.setSelectedIndices(new int[] {});
 		}
 	}
-	
-	// Pide interactivamente una carpeta para coger vídeos
+
+	// Pide interactivamente una carpeta para coger vï¿½deos
 	// (null si no se selecciona)
 	private static File pedirCarpeta() {
 		// TODO: Pedir la carpeta usando JFileChooser
 		return null;
 	}
 
-		private static String ficheros;
-		private static String path;
-	/** Ejecuta una ventana de VideoPlayer.
-	 * El path de VLC debe estar en la variable de entorno "vlc".
-	 * Comprobar que la versión de 32/64 bits de Java y de VLC es compatible.
-	 * @param args	Un array de dos strings. El primero es el nombre (con comodines) de los ficheros,
-	 * 				el segundo el path donde encontrarlos.  Si no se suministran, se piden de forma interactiva. 
+	private static String ficheros;
+	private static String path;
+
+	/**
+	 * Ejecuta una ventana de VideoPlayer. El path de VLC debe estar en la variable
+	 * de entorno "vlc". Comprobar que la versiï¿½n de 32/64 bits de Java y de VLC es
+	 * compatible.
+	 * 
+	 * @param args Un array de dos strings. El primero es el nombre (con comodines)
+	 *             de los ficheros, el segundo el path donde encontrarlos. Si no se
+	 *             suministran, se piden de forma interactiva.
 	 */
 	public static void main(String[] args) {
-		// Para probar carga interactiva descomentar o comentar la línea siguiente:
+		// Para probar carga interactiva descomentar o comentar la lï¿½nea siguiente:
 		args = new String[] { "*Pentatonix*.mp4", "test/res/" };
 		if (args.length < 2) {
-			// No hay argumentos: selección manual
+			// No hay argumentos: selecciï¿½n manual
 			File fPath = pedirCarpeta();
-			if (fPath==null) return;
+			if (fPath == null)
+				return;
 			path = fPath.getAbsolutePath();
-			// TODO : Petición manual de ficheros con comodines (showInputDialog)
+			// TODO : Peticiï¿½n manual de ficheros con comodines (showInputDialog)
 			// ficheros = ???
 		} else {
 			ficheros = args[0];
 			path = args[1];
 		}
-		
+
 		// Inicializar VLC.
 		// Probar con el buscador nativo...
 		boolean found = new NativeDiscovery().discover();
-    	// System.out.println( LibVlc.INSTANCE.libvlc_get_version() );  // Visualiza versión de VLC encontrada
-    	// Si no se encuentra probar otras opciones:
-    	if (!found) {
+		// System.out.println( LibVlc.INSTANCE.libvlc_get_version() ); // Visualiza
+		// versiï¿½n de VLC encontrada
+		// Si no se encuentra probar otras opciones:
+		if (!found) {
 			// Buscar vlc como variable de entorno
-			String vlcPath = System.getenv().get( "vlc" );
-			if (vlcPath==null) {  // Poner VLC a mano
-	        	System.setProperty("jna.library.path", "c:\\Program Files\\videolan\\VLC");
-			} else {  // Poner VLC desde la variable de entorno
-				System.setProperty( "jna.library.path", vlcPath );
+			String vlcPath = System.getenv().get("vlc");
+			if (vlcPath == null) { // Poner VLC a mano
+				System.setProperty("jna.library.path", "c:\\Program Files\\videolan\\VLC");
+			} else { // Poner VLC desde la variable de entorno
+				System.setProperty("jna.library.path", vlcPath);
 			}
 		}
-    	
-    	// Lanzar ventana
-		SwingUtilities.invokeLater( new Runnable() {
+
+		// Lanzar ventana
+		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				miVentana = new VideoPlayer();
-				// Descomentar estas dos líneas para ver un vídeo de ejemplo
+				// Descomentar estas dos lï¿½neas para ver un vï¿½deo de ejemplo
 				// miVentana.listaRepVideos.ficherosLista = new ArrayList<File>();
-				// miVentana.listaRepVideos.ficherosLista.add( new File("test/res/[Official Video] Daft Punk - Pentatonix.mp4") );				
-				miVentana.setVisible( true );
-				miVentana.listaRepVideos.add( path, ficheros );
+				// miVentana.listaRepVideos.ficherosLista.add( new File("test/res/[Official
+				// Video] Daft Punk - Pentatonix.mp4") );
+				miVentana.setVisible(true);
+				miVentana.listaRepVideos.add(path, ficheros);
 				miVentana.listaRepVideos.irAPrimero();
 				miVentana.lanzaVideo();
 			}
 		});
 	}
-	
+
 }
